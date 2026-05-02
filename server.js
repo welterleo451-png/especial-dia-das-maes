53eb808c208896fe3067f1d917d92899e83ac154
Leonardo Welter <welterleo451@gmail.com>
2026-05-02 04:20:51 -0300

    Set body parser limits to 50mb
    Increase JSON and URL-encoded body size limits to 50mb.

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ const supabase = createClient(
 );
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('public'));
 
 const pagamentosConfirmados = new Map();
